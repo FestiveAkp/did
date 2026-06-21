0adcc3340c2577cfdc4b4039a6f24021a68fd272
Akash Pillai <akashp807@gmail.com>
2026-06-21 15:27:11 -0500

    Implement basic bubbletea program

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	count int
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		case "k":
+			m.count++
+		case "j":
+			m.count--
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	return fmt.Sprintf("Count: %d\n\nj/k to change, q to quit\n", m.count)
+}
 
 func main() {
-	fmt.Println("hello world")
+	p := tea.NewProgram(model{})
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 }
